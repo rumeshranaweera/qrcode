21155fd9b4886ddad94a24fd9c9537418ede0daa
rum <rumeshranaweera99@gmail.com>
2023-09-15 16:25:20 +0530

    Initial commit

---
+++ src/lib/context-api.tsx
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+const useInput = createContext(null);
+
+const ContextAPI = () => {
+  //   return <useInput.Provider value={}>ContextAPI</useInput.Provider>;
+};
+
+export default ContextAPI;
---
+++ src/theme/themeConfig.ts
@@ -0,0 +1,12 @@
+import type { ThemeConfig } from "antd";
+
+const theme: ThemeConfig = {
+  token: {
+    fontSize: 16,
+    colorPrimary: "#fda530",
+    padding: 100,
+    colorText: "#52c41a",
+  },
+};
+
+export default theme;
